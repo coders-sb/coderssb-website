db7fd6d57acfadd691172c0e6d1d85404f395e4c
David Wang <david.wang5766@gmail.com>
2023-01-07 16:29:14 -0800

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "65b79403c85f31015307";
+/******/ 	var hotCurrentHash = "e501f9429c955faa616f";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
